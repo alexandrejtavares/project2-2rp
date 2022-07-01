8c70c0a8efb7265db3767f0c8a6f4c6655261860
Alexandre Tavares <alexandrejtavares@yahoo.com.br>
2022-07-01 03:37:34 -0300

    Cleaning code

--- main.py
+++ main.py
@@ -14,61 +14,62 @@ vm_name = "teste-zeppelin"
 @logger.catch
 def vagrant_up() -> bool:
     command = "vagrant up"
-    print (command)
 
     try:
-        logger.info("Inicializando máquina virtual.")
+        logger.info("VAGRANT - Initializing virtual machine.")
         subprocess.run(command)
         return True
     except:    
-        logger.error("VAGRANT - Erro ao iniciar a máquina virtual.")
+        logger.error("VAGRANT - Error initializing virtual machine.")
         return False
 
 #----------------------------------------------------------------------
 @logger.catch
 def install_zeppelin():
     '''
-    Install Apache Zeppelin in the VM.
+    Installs Apache Zeppelin in the VM.
     '''
     try:
-        logger.info(f"Instalando o Apache Zeppelin na máquina virtual.")
+        logger.info(f"Installing Apache Zeppelin in the virtual machine.")
         ssh_command = "sudo yum install -y zeppelin"
         command = f"vagrant ssh -c '{ssh_command}' "
-        logger.info(f"Apache Zeppelin instalado com sucesso.")
+        subprocess.run(command)
+        logger.info(f"Apache Zeppelin installed successfully.")
     except:    
         logger.debug(f"Command: {command}")
-        logger.error("Erro ao instalar o Apache Zeppelin.")
+        logger.error("Error installing Apache Zeppelin.")
 
 #----------------------------------------------------------------------
 @logger.catch
 def install_java():
     '''
-    Install Java in the VM.
+    Installs Java in the VM.
     '''
     try:
-        logger.info(f"Instalando o Java na máquina virtual.")
+        logger.info(f"Installing java in the virtual machine.")
         ssh_command = "sudo yum install -y java"
         command = f"vagrant ssh -c '{ssh_command}' "
-        logger.info(f"Java instalado com sucesso.")
+        subprocess.run(command)
+        logger.info(f"Java was installed successfully")
     except:    
         logger.debug(f"Command: {command}")
-        logger.error("Erro ao instalar o Java.")
+        logger.error("Error installing Java.")
 
+if __name__ == "__main__":
 
-# Get program path
-app_dir = path.dirname(path.realpath(__file__))
+    # Get program path
+    app_dir = path.dirname(path.realpath(__file__))
 
-# Vagrant path
-vagrant_path = path.join(app_dir, "vagrant")
+    # Vagrant path
+    vagrant_path = path.join(app_dir, "vagrant")
 
-# Change workdir to vagrant path
-os.chdir(vagrant_path)
+    # Change workdir to vagrant path
+    os.chdir(vagrant_path)
 
-# Inicializa a máquina virtual
-vagrant_up()
+    # Inicializa a máquina virtual
+    if vagrant_up():
+        # Instala o Java
+        install_java()
 
-# Instala o Java
-install_java()
-
-# Instala o Apache Zeppelin
-install_zeppelin()
+        # Instala o Apache Zeppelin
+        install_zeppelin()
