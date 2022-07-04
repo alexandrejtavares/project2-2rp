261db3536859b7d1522ceb8d4095da243020cb97
Alexandre Tavares <alexandrejtavares@yahoo.com.br>
2022-07-04 01:42:20 -0300

    ADD: Install of Apache Zeppelin

--- main.py
+++ main.py
@@ -27,65 +27,93 @@ def vagrant_up() -> bool:
 
 #----------------------------------------------------------------------
 @logger.catch
-def verify_wget(vagrant_cmd:str) -> bool:
+def verify_wget(vagrant_cmd: str) -> bool:
     '''
     Verify if wget is installed.
     '''
     try:
         logger.info("Verifying if wget is installed...")
         ssh_cmd = "wget --version"
-        ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", stdout=PIPE)
-        if ret.returncode != 0:
-            logger.info("wget is not installed.")
-            install_wget(vagrant_cmd)
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'")
+        return ret.returncode == 0
     except Exception as err:    
         logger.error(f"Error verifying wget: {err}.")
+        return False
 
 #----------------------------------------------------------------------
 @logger.catch
-def install_wget(vagrant_cmd:str):
+def verify_java(vagrant_cmd: str) -> bool:
+    '''
+    Verify if Java is installed.
+    '''
+    try:
+        logger.info("Verifying if Java is installed...")
+        ssh_cmd = "java -version"
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'")
+        return ret.returncode == 0
+    except Exception as err:    
+        logger.error(f"Error verifying Java version: {err}.")
+        return False
+
+#----------------------------------------------------------------------
+@logger.catch
+def verify_zeppelin(vagrant_cmd: str) -> bool:
+    '''
+    Verify if Apache Zeppelin is installed as service.
+    '''
+    try:
+        logger.info("Verifying if Apache Zeppelin is installed...")
+        ssh_cmd = "sudo systemctl status zeppelin"
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'")
+        return ret.returncode == 0
+    except Exception as err:    
+        logger.error(f"Error verifying Apache Zeppelin service: {err}.")
+        return False
+
+#----------------------------------------------------------------------
+@logger.catch
+def install_wget(vagrant_cmd: str):
     '''
     Installs wget in the VM.
     '''
     try:
         logger.info(f"Installing wget in the virtual machine...")
         ssh_cmd = "sudo yum install -y wget"
-        subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
         logger.info(f"wget installed successfully.")
     except Exception as err:    
         logger.error(f"Error installing wget: {err}.")
 
 #----------------------------------------------------------------------
 @logger.catch
-def update_yum(vagrant_cmd:str):
+def update_yum(vagrant_cmd: str):
     '''
     Update yum.
     '''
     try:
         logger.info(f"Updating yum...")
         ssh_cmd = "sudo yum update -y"
-        subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
         logger.info(f"yum updated successfully.")
     except Exception as err:    
         logger.error(f"Error updating yum: {err}.")   
 
 #----------------------------------------------------------------------
 @logger.catch
-def tar_extract(vagrant_cmd: str, tar_file:str, path_destiny:str) -> bool:
+def tar_extract(vagrant_cmd: str, tar_file: str, path_destiny: str) -> bool:
     logger.info(f"Extracting {tar_file}...")
-    ssh_cmd = f"sudo tar xf {tar_file} -C {path_destiny}"
-    ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'")
+    ssh_cmd = f"sudo tar xvf {tar_file} -C {path_destiny}"
+    ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'")
     return bool(ret.returncode)
 
 #----------------------------------------------------------------------
 @logger.catch
-def wget_download(vagrant_cmd:str, url:str, path_destiny:str) -> bool:   
+def wget_download(vagrant_cmd: str, url: str, path_destiny: str) -> bool:   
     # Checks if wget is installed, otherwise installs it.
     try:
-        if not verify_wget:
-            install_wget()
+        logger.info(f"Starting download of the url: {url}...")
         ssh_cmd = f"wget {url} -P {path_destiny}"
-        ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
     except Exception as err:
         logger.error(f"Error downloading the file using wget: {err}.")        
 
@@ -103,29 +131,29 @@ def install_java(vagrant_cmd: str):
        
         logger.info("Installing JDK...")
         ssh_cmd = "sudo rpm -ivh jdk-18_linux-x64_bin.rpm"
-        ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
         
         logger.info(f"Including JDK's path in PATH variable...")
         ssh_cmd = f"export PATH=\"$PATH:/usr/java/latest/bin\""
-        ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
 
         logger.info(f"Setting JAVA_HOME variable in bash_profile file...")
         ssh_cmd = f"export PATH=\"$PATH:/usr/java/latest\" >> ~/.bash_profile"
-        ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
 
         logger.info(f"Setting JRE_HOME variable in bash_profile file...")
         ssh_cmd = f"echo \"export JRE_HOME=/usr/java/latest/bin\" >> ~/.bash_profile"
-        ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
 
         logger.info(f"Executing bash_profile to set variables...")
         ssh_cmd = f"source ~/.bash_profile"
-        ret = subprocess.run(f"{vagrant_cmd}'{ssh_cmd}'", check=True)
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
     except Exception as err:    
         logger.error(f"Error installing Java: {err}.")
 
 #----------------------------------------------------------------------
 @logger.catch
-def install_zeppelin(vagrant_cmd: str):
+def install_zeppelin(vagrant_cmd: str, install_dir: str):
     '''
     Installs Apache Zeppelin in the VM.
     '''
@@ -135,39 +163,100 @@ def install_zeppelin(vagrant_cmd: str):
         
         logger.info("Extracting Apache Zeppelin...")
         tar_extract(vagrant_cmd, "zeppelin-*-bin-all.tgz", "/opt")
-
-        # To be continued...
-
+       
+        logger.info(f"Moving Apache Zeppelin binaries to final directory...")
+        ssh_cmd = f"sudo mv /opt/zeppelin-*-bin-all {install_dir}"
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
     except Exception as err:    
         logger.error(f"Error installing Apache Zeppelin: {err}.")
 
+#----------------------------------------------------------------------
+@logger.catch
+def start_zeppelin(vagrant_cmd: str, service_name: str):
+    '''
+    Start Apache Zeppelin service.
+    '''  
+    try:
+        logger.info(f"Starting Apache Zeppelin service...")
+        ssh_cmd = f"sudo systemctl start {service_name}"
+        ret = subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
+    except Exception as err:
+        logger.error(f"Error starting Apache Zeppelin service: {err}.")   
+#----------------------------------------------------------------------
+@logger.catch
+def create_zeppelin_service(vagrant_cmd: str, install_dir: str, service_name: str, service_file_path: str):
+    '''
+    Create Apache Zeppelin service.
+    '''    
+    try:
+        logger.info(f"Creating Apache Zeppelin service user...")
+        ssh_cmd = f"sudo adduser -d {install_dir} -s /sbin/nologin {service_name}"
+        subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'")
+
+        logger.info(f"Providing ownership of the files to the Zeppelin user....")
+        ssh_cmd = f"sudo chown -R {service_name}:{service_name} {install_dir}"
+        subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
+
+        cur_path = path.dirname(path.abspath(__file__))
+        service_file_path = path.join(cur_path, service_file_path)
+        with open(service_file_path) as buf:
+            service_str = buf.read()
+        
+        service_str = service_str.replace("{install_dir}", install_dir)
+        service_str = service_str.replace("{user}", service_name)
+        service_str = service_str.replace("{group}", service_name)
+        service_str = service_str.strip()
+        
+        logger.info(f"Creating Apache Zeppelin .service file by the file {service_name}.service...")
+        ssh_cmd = f"sudo echo '{service_str}' > {service_name}.service"
+        cmd = f"{vagrant_cmd} \"{ssh_cmd}\""
+        subprocess.run(cmd, check=True)
+        
+        ssh_cmd = f"sudo mv {service_name}.service /etc/systemd/system/{service_name}.service"
+        subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
+        
+        start_zeppelin(vagrant_cmd, service_name)
+
+        logger.info(f"Enable Zeppelin service to automatically start at boot time...")
+        ssh_cmd = f"sudo systemctl enable {service_name}"
+        subprocess.run(f"{vagrant_cmd} '{ssh_cmd}'", check=True)
+    except Exception as err:    
+        logger.error(f"Error creating Apache Zeppelin service: {err}.")
 
 if __name__ == "__main__":
+
+    zeppelin_install_dir = "/opt/zeppelin"
+    zeppelin_service_name = "zeppelin"
+    zeppelin_service_file_path = path.join("vagrant", "zeppelin.service")
 
     # Get program path
     app_dir = path.dirname(path.realpath(__file__))
 
     # Vagrant path
     vagrant_path = path.join(app_dir, "vagrant")
-
     # Change workdir to vagrant path
-    os.chdir(vagrant_path)
 
-    vagrant_cmd = f"vagrant ssh -c "
+    os.chdir(vagrant_path)
+    vagrant_cmd = f"vagrant ssh -c"
 
     # Inicializa a máquina virtual
-    if vagrant_up():
+    vagrant_up()
 
-        # Updating yum
-        update_yum()
+    # Updating yum
+    update_yum(vagrant_cmd)
 
-        # Verifying if wget is installed
-        if not verify_wget(vagrant_cmd):
-            # Install wget
-            install_wget()
+    # Verify if wget is installed
+    if not verify_wget(vagrant_cmd):
+        # Install wget
+        install_wget(vagrant_cmd)
 
-        # Instala o Java
+    # Verify if Java is installed
+    if not verify_java(vagrant_cmd):
         install_java(vagrant_cmd)
 
-        # Instala o Apache Zeppelin
-        install_zeppelin(vagrant_cmd)
+    # Verify if Apache Zeppelin is installed
+    if not verify_zeppelin(vagrant_cmd):
+        install_zeppelin(vagrant_cmd, zeppelin_install_dir)
+
+    # Create Apache Zeppelin Service
+    create_zeppelin_service(vagrant_cmd, zeppelin_install_dir, zeppelin_service_name, zeppelin_service_file_path)
--- readme.md
+++ readme.md
@@ -5,6 +5,14 @@
 - Criar um script Vagrant que suba uma máquina CentOS 7.x com 2 CPUs (2 cores de processador), 4 GB de memória RAM e 50gb de HD chamada “teste-zeppelin”. O acesso a ela deve ser através de uma chave privada, não com senha.  
 - Criar um programa em python que faça a instalação do Java e do Apache Zeppelin nessa máquina recém criada. Além disso, o programa deve subir o webserver do Zeppelin na porta 8888 e fazer uma criação de usuários a partir da lista presente no arquivo Lista_Usuarios_Zeppelin.txt.  
 
+## Ferramentas
+- Vagrant 2.2.19
+- Oracle VirtualBox 6.1.20
+- CentOS 7
+- JDK 1.8.0_332
+- Wget 1.14
+- 
+
 ## Como Executar
 Execute o comando:
 
@@ -23,4 +31,6 @@ O programa executará os passos abaixo:
 
 - https://www.vagrantup.com/docs  
 - https://zeppelin.apache.org/docs  
+- https://www.oracle.com/java/technologies/jdk-script-friendly-urls/  
+    
  
